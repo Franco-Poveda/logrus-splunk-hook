4cb41777b1f3fc60017b7d81308b6a5da4af5bdf
Franco-Poveda <franco.poveda@gmail.com>
2018-11-25 02:34:50 -0300

    basic hook implementation

---
+++ hook.go
@@ -0,0 +1,33 @@
+package splunk
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+// Hook is a logrus hook for splunk
+type Hook struct {
+	Client *Client
+	levels []logrus.Level
+}
+
+// NewHook creates new hook
+// client - splunk client instance (use NewClient)
+// level - log level
+func NewHook(client *Client, levels []logrus.Level) *Hook {
+
+	return &Hook{client, levels}
+}
+
+// Fire triggers a splunk event
+func (h *Hook) Fire(entry *logrus.Entry) error {
+	err := h.Client.Log(
+		entry,
+	)
+	return err
+}
+
+// Levels Required for logrus hook implementation
+func (h *Hook) Levels() []logrus.Level {
+	return h.levels
+
+}
